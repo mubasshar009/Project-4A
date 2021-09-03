c375270e0db4531e155374ba469ed602b6b9926a
Mubashar Hassan <mubasshar009@gmail.com>
2021-09-03 21:31:02 +0500

    Final Update

--- src/Components/Animation.js
+++ src/Components/Animation.js
@@ -1,80 +1,119 @@
 import useWebAnimations from "@wellyshen/use-web-animations";
-import alice from '../images/alice-hungry_colors.png'
+import ali from '../images/alice-hungry_colors.png'
 import ponyHair from '../images/alice-hungry-ponytail.png'
 import table from '../images/table.png'
 import drinkMe from '../images/drink-me_fg_small.png'
 import bottle_b from '../images/doily_small.png';
 import eatMe from '../images/sprite_cupcake_small.png'
-import { useState } from "react";
+import { useState, useEffect } from "react";
 const Animation = () => {
-    const [cake, setCake] = useState(false);
-    const [timeState, setTimeState] = useState();
+    const [cake, setCake] = useState('');
+    const [alice, setAlice] = useState(true);
     const { ref: aliceRef, getAnimation: getAnimationforAlice } = useWebAnimations({
         id: 'alice-tot',
         autoPlay: false,
         keyframes:
-            [{ transform: 'translate(-50%, -50%) scale(.5)' },
-            { transform: 'translate(-50%, -50%) scale(2)' }]
+            { transform: ['translate(-50%, -50%) scale(.5)', 'translate(-50%, -50%) scale(2)'] }
         ,
-        animationOptions: {
-
-            duration: 8000,
-            easing: 'ease-in-out',
-            fill: 'both'
+        animationOptions:
+        {
+            direction: 'normal',
+            duration: 18000,
+            iterations: Infinity,
         }
+        ,
+
+        onReady: ({ animation }) => animation.currentTime = animation.effect.getTiming().duration / 2,
+        //onUpdate: ({ animation }) => console.log((animation.timeline.currentTime))
+
     })
 
     const { ref: cakeRef, getAnimation: getAnimationforCake } = useWebAnimations({
         autoPlay: false,
+        id: 'eatMe',
         keyframes: { transform: ['translateY(-100%)'] },
         animationOptions: {
-            duration: 400,
+            duration: 5000,
             easing: 'steps(5, end)',
-            direction: 'reverse',
-            iterations: Infinity,
-            playbackRate: 1,
+
+
+            playbackRate: 5,
         },
+        onReady: ({ animation }) => (animation.currentTime = animation.effect.getTiming().duration),
+    })
+    const { ref: liquid, getAnimation: getAnimationforLiquid } = useWebAnimations({
+        autoPlay: false,
+        id: 'liquid',
+        keyframes: [
+            { height: '100%' },
+            { height: '0%' }
+        ],
+        animationOptions: {
+            fill: 'forwards',
+            duration: 9000,
+            iterations: Infinity
+        },
+        onReady: ({ animation }) => (animation.currentTime = animation.effect.getTiming().duration / 2),
     })
     const AliceChange = () => {
 
     }
     const eatMeChange = () => {
-        const animation = getAnimationforCake;
-        console.log(animation)
-        setTimeState(animation.effect.getTiming().duration / 2);
+        //Grow Alice
+        setTimeout(() => getAnimationforAlice().playbackRate = 1, 1)
+        setTimeout(() => getAnimationforAlice().play(), 200)
+        setTimeout(() => getAnimationforAlice().pause(), 500)
 
-        animation.pause();
+        if (getAnimationforAlice().currentTime > 12000) {
+            alert("Poor Alice Has Been Died")
+        }
+        //Cake Animation
+        setTimeout(() => getAnimationforCake().play(), 400);
+        setTimeout(() => getAnimationforCake().pause(), 1000);
+    }
+    const handleliquid = () => {
+        //Shrink Alice
+        setTimeout(() => getAnimationforAlice().playbackRate = -1, 1)
+        setTimeout(() => getAnimationforAlice().play(), 20)
+        setTimeout(() => getAnimationforAlice().pause(), 900)
+        //Animation For Liquid
 
-        // if (setCake) {
-        //     animation.pause();
+        setTimeout(() => getAnimationforLiquid().updatePlaybackRate *= 2)
+        setTimeout(() => getAnimationforLiquid().play(), 20);
+        setTimeout(() => getAnimationforLiquid().pause(), 400);
 
-        // } else {
-        //     animation.play()
-        //     setCake(true);
-        // }
-
+        //Time
+        if (getAnimationforLiquid().currentTime > 8600) {
+            alert("Poor Alice Dies")
+        }
     }
 
+
+    console.log(cake);
     return (
+
+
         <div className="main">
-            <div ref={aliceRef} className="alice" id="alice-tot">
-                <img id="alice-skelton" src={alice} alt="" />
+            <div ref={aliceRef} className="alice" onClick={AliceChange} id="alice-tot">
+                <img id="alice-skelton" src={ali} alt="" />
                 <img id="hair" src={ponyHair} alt="" />
             </div>
             <div className="forground">
                 <div className="table">
                     <img id="table-ad" src={table} alt="" />
                 </div>
-                <div className="drinkme">
+                <div className="drinkme" onClick={handleliquid}>
                     <img src={drinkMe} id="drinkMe" alt="" />
+                    <div ref={liquid} id="liquid" ></div>
                     <img src={bottle_b} id="bottle_b" alt="" />
 
                 </div>
                 <div className="eat_me">
-                    <img ref={cakeRef} src={eatMe} id="eatMe" alt="" />
+                    <img ref={cakeRef} onClick={eatMeChange} src={eatMe} id="eatMe" alt="" />
                 </div>
             </div>
         </div>
+
     )
 }
 
